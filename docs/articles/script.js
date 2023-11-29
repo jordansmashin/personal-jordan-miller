b1eb0febaeaf41332beb7512796251c3e9ec73e1
Jordan Miller <jmillersmashin@gmail.com>
2023-11-28 20:40:21 -0500

    finishing up my major enhancement and the article on it

--- docs/articles/script.js
+++ docs/articles/script.js
@@ -25,7 +25,7 @@ function generatePassword() {
         chars += specialChars;
     }
 
-    password = ''; 
+    password = ''; // Reset the password variable
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * chars.length);
         password += chars.charAt(randomIndex);
